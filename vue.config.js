6579f217609628ac459ac16f215c13da08666fe7
liuzhanghao <3443718488@qq.com>
2022-09-08 12:29:02 +0800

    打包修改

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
 	publicPath: isDev ? '' : './dist',
-	outputDir: '../dist',
+	outputDir: './dist', // 打包dist 所在路径
 	lintOnSave: false,
 	productionSourceMap: false,
 	transpileDependencies: ['simple-mind-map'],
