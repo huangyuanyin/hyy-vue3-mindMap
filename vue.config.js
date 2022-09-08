e2c07b42a89cd7e35110c684e6dba9fb75614971
liuzhanghao <3443718488@qq.com>
2022-09-08 13:11:44 +0800

    修改 配置打包生成的静态资源路径

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-	publicPath: isDev ? '' : './dist',
+	publicPath: isDev ? '' : './',
 	outputDir: './dist', // 打包dist 所在路径
 	lintOnSave: false,
 	productionSourceMap: false,
