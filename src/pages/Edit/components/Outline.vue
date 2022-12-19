057fc5d775d7a50a6205d59d76eab5085f911849
黄原寅 <3443718488@qq.com>
2022-12-19 16:42:40 +0800

    feat 支持在大纲直接编辑节点文本内容

--- src/pages/Edit/components/Outline.vue
+++ src/pages/Edit/components/Outline.vue
@@ -1,6 +1,20 @@
 <template>
   <Sidebar ref="sidebar" :title="$t('outline.title')">
-    <el-tree :data="data" :props="defaultProps" default-expand-all></el-tree>
+    <el-tree class="outlineTree" :data="data" :props="defaultProps" :expand-on-click-node="false" default-expand-all>
+      <template #default="{ node, data }">
+        <span class="customNode">
+          <span
+            class="nodeEdit"
+            :key="getKey()"
+            contenteditable="true"
+            @keydown.stop
+            @keyup.stop
+            @blur="onBlur($event, node)"
+            v-html="node.label"
+          ></span>
+        </span>
+      </template>
+    </el-tree>
   </Sidebar>
 </template>
 
@@ -9,24 +23,38 @@
  * @Author: 黄原寅
  * @Desc: 大纲内容
  */
-import { onMounted, ref, nextTick } from 'vue'
+import { onMounted, ref, defineProps, nextTick } from 'vue'
 import Sidebar from './Sidebar'
 import { mapState } from 'vuex'
 import bus from '@/utils/bus.js'
 
+const props = defineProps({
+  mindMap: {
+    type: Object
+  }
+})
 const sidebar = ref(null)
 const data = ref([])
 const defaultProps = ref({
   label(data) {
-    return data.data.text
+    return data.data.text.replaceAll(/\n/g, '</br>')
   }
 })
 
 onMounted(() => {
+  console.log('props.mindMap', props.mindMap)
   bus.on('data_change', data2 => {
-    data.value = [data2]
+    data.value = [props.mindMap.renderer.renderTree]
   })
 })
+
+const onBlur = (e, node) => {
+  node.data._node.setText(e.target.innerText)
+}
+
+const getKey = () => {
+  return Math.random()
+}
 </script>
 
 <script>
@@ -48,4 +76,46 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.customNode {
+  width: 100%;
+  overflow-x: auto;
+  &::-webkit-scrollbar {
+    width: 7px;
+    height: 7px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 7px;
+    background-color: rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+  }
+  &::-webkit-scrollbar-track {
+    box-shadow: none;
+    background: transparent;
+    display: none;
+  }
+  .nodeEdit {
+    outline: none;
+  }
+}
+.outlineTree {
+  /deep/ .el-tree-node__content {
+    height: auto;
+    margin: 5px 0;
+    .el-tree-node__expand-icon.is-leaf {
+      position: relative;
+      &::after {
+        position: absolute;
+        content: '';
+        width: 5px;
+        height: 5px;
+        border-radius: 50%;
+        background-color: #c0c4cc;
+        left: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
+  }
+}
+</style>
