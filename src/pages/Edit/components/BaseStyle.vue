9a7be2e65a4ac00b07282110ceb3c9f8acc463c6
黄原寅 <3443718488@qq.com>
2023-02-16 16:08:27 +0800

    fix 修复设置了水印页面刷新后生效的问题

--- src/pages/Edit/components/BaseStyle.vue
+++ src/pages/Edit/components/BaseStyle.vue
@@ -573,6 +573,7 @@ export default {
         this.mindMap.watermark.updateWatermark({
           ...config
         })
+        this.data.config = this.data.config || {}
         this.data.config.watermarkConfig = this.mindMap.getConfig('watermarkConfig')
         storeConfig({
           config: this.data.config
