013c6dcf0ad4ae0bfc88dbae66ad2c6596ee7d23
黄原寅 <3443718488@qq.com>
2023-12-05 14:05:19 +0800

    fix 修复节点内容为html标签时大纲无法显示和编辑的问题

--- src/pages/Edit/components/Outline.vue
+++ src/pages/Edit/components/Outline.vue
@@ -40,7 +40,7 @@
  */
 import { onMounted, ref, watch, computed, nextTick, onBeforeMount } from 'vue'
 import { mapState, useStore } from 'vuex'
-import { nodeRichTextToTextWithWrap, textToNodeRichTextWithWrap, getTextFromHtml, createUid } from 'simple-mind-map/src/utils'
+import { nodeRichTextToTextWithWrap, textToNodeRichTextWithWrap, getTextFromHtml, createUid, htmlEscape } from 'simple-mind-map/src/utils'
 import bus from '@/utils/bus.js'
 
 const props = defineProps({
@@ -126,7 +126,8 @@ const refresh = () => {
   data.value = props.mindMap.getData()
   data.value.root = true // 标记根节点
   let walk = root => {
-    const text = (root.data.richText ? nodeRichTextToTextWithWrap(root.data.text) : root.data.text).replaceAll(/\n/g, '<br>')
+    let text = (root.data.richText ? nodeRichTextToTextWithWrap(root.data.text) : root.data.text).replaceAll(/\n/g, '<br>')
+    text = htmlEscape(text)
     root.textCache = text // 保存一份修改前的数据，用于对比是否修改了
     root.label = text
     root.uid = root.data.uid
